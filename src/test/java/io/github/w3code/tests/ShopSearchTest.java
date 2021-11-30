0ce194e05a8b88d87057fce88dd41739a07ba1f5
cbet <npocbet@gmail.com>
2021-11-30 21:36:30 +0300

    some comments added just to make 9th hw

--- src/test/java/io/github/w3code/tests/ShopSearchTest.java
+++ src/test/java/io/github/w3code/tests/ShopSearchTest.java
@@ -11,6 +11,7 @@ import java.util.stream.Stream;
 
 public class ShopSearchTest extends TestBase {
 
+    //some npocbet's comments 4 9th homework
     @EnumSource(ToolItems.class)
     @Tag("Blocker")
     @DisplayName("Power Tools search")
